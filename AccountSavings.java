d7221ef08aab8fde99d5950d48bf09d2d5518c4b
Shanmukhi6630 <168158558+Shanmukhi6630@users.noreply.github.com>
2024-09-29 22:41:43 +0530

    Create AccountSavings.java

---
+++ AccountSavings.java
@@ -0,0 +1,45 @@
+public class SavingsAccount 
+{
+    private static double annualInterestRate;
+    private double savingsBalance;
+    public SavingsAccount(double initialBalance) 
+    {
+        if (initialBalance >= 0) 
+        {
+            this.savingsBalance = initialBalance;
+        } 
+        else 
+        {
+            this.savingsBalance = 0;
+        }
+    }
+    public void calculateMonthlyInterest() 
+    {
+        double monthlyInterest = (savingsBalance * annualInterestRate) / 12;
+        savingsBalance += monthlyInterest;
+    }
+    public static void modifyInterestRate(double newRate) 
+    {
+        annualInterestRate = newRate;
+    }
+    public double getSavingsBalance()
+    {
+        return savingsBalance;
+    }
+
+}
+
+public class AccountSavings 
+{
+    public static void main(String[] args) 
+    {
+        SavingsAccount.modifyInterestRate(0.04);
+        SavingsAccount account1 = new SavingsAccount(1000.00);
+        account1.calculateMonthlyInterest();
+        System.out.println("Savings Balance after interest: " + account1.getSavingsBalance());
+        SavingsAccount.modifyInterestRate(0.05);
+        SavingsAccount account2 = new SavingsAccount(2000.00);
+        account2.calculateMonthlyInterest();
+        System.out.println("Savings Balance after interest: " + account2.getSavingsBalance());
+    }
+}
